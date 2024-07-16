65803f8e7e843a48ee4cca30e444f60e16bb467a
justinhSE <justinh.tech1@gmail.com>
2024-07-16 17:50:01 -0400

    attempt

--- background.js
+++ background.js
@@ -1,16 +1,32 @@
-// Import the necessary Firebase SDKs
-import { doc, setDoc, arrayUnion } from "firebase/firestore";
-import { db } from './firebase.js'; // Import db from firebase.js
+import { initializeApp } from "./firebase/app";
+import { getAnalytics } from "./firebase/analytics";
+import { getFirestore } from "./firebase/firestore"; // Import Firestore functions
+require("dotenv").config();
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyABoHieDK0eaaVrwAPpbpnIUw1fcrEQpPA",
+    authDomain: "goanalytics-20-20-vision.firebaseapp.com",
+    projectId: "goanalytics-20-20-vision",
+    storageBucket: "goanalytics-20-20-vision.appspot.com",
+    messagingSenderId: "308906954285",
+    appId: "1:308906954285:web:1ca706a6fe2ee249de32a4",
+    measurementId: "G-BTHCNN9D6V"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 // background.js
-chrome.identity.getProfileUserInfo(function(userInfo) {
+chrome.identity.getProfileUserInfo(function (userInfo) {
     const userId = userInfo.id;
 
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         console.log('Mouse position received:', message);
         //from before db
-            // Store the mouse position in the local storage
-        chrome.storage.local.get(['mousePositions'], function(result) {
+        // Store the mouse position in the local storage
+        chrome.storage.local.get(['mousePositions'], function (result) {
             const mousePositions = result.mousePositions || [];
             mousePositions.push(message);
             chrome.storage.local.set({ mousePositions: mousePositions });
